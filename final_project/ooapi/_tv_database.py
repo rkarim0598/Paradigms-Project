814430552c1e4a0b7c1146f20533b41ecbb55034
Ryker Campbell McIntyre <rmcinty3@student06.cse.nd.edu>
2018-11-05 18:11:18 -0500

    TABS

--- final_project/ooapi/_tv_database.py
+++ final_project/ooapi/_tv_database.py
@@ -22,40 +22,40 @@ class _tv_database:
 			line = line.rstrip()
 			components = line.split("::")
 
-                        self.tvshows[components[0]] = {}
+			self.tvshows[components[0]] = {}
 			self.tvshows[components[0]]['name'] = components[1]
-                        self.tvshows[components[0]]['genres'] = components[2]
-                        self.tvshows[components[0]]['site'] = components[3]
-                        self.tvshows[components[0]]['rating'] = components[4]
-                        self.tvshows[components[0]]['image'] = components[5]
-                        self.tvshows[components[0]]['summary'] = components[6]
-                        
+			self.tvshows[components[0]]['genres'] = components[2]
+			self.tvshows[components[0]]['site'] = components[3]
+			self.tvshows[components[0]]['rating'] = components[4]
+			self.tvshows[components[0]]['image'] = components[5]
+			self.tvshows[components[0]]['summary'] = components[6]
+		
 		return self.tvshows
-        
-        def get_show(self, sid):
-                sid = str(sid)
-                
-                if sid in self.tvshows:
-                        output = self.tvshows[sid]
-                        output['result'] = 'success'
-                else:
-                        output = { 'result': 'failure' }
-
-                return output
-
-        def set_show(self, sid, d = {}):
-                if len(d) == 6:
-                        self.tvshows[sid] = d
-
-        def get_shows(self):
-                return [i for i in self.tvshows.keys()]
-
-        def reset_shows(self):
-                self.tvshows = load_tvshows('shows.txt')
-
-        def delete_shows(self):
-                self.tvshows = {}
-
-        def delete_show(self, sid):
-                if sid in self.tvshows:
-                        self.tvshows.pop(sid)
+		
+	def get_show(self, sid):
+		sid = str(sid)
+		
+		if sid in self.tvshows:
+			output = self.tvshows[sid]
+			output['result'] = 'success'
+		else:
+			output = { 'result': 'failure' }
+		
+		return output
+	
+	def set_show(self, sid, d = {}):
+		if len(d) == 6:
+	    	self.tvshows[sid] = d
+	
+	def get_shows(self):
+		return [i for i in self.tvshows.keys()]
+	
+	def reset_shows(self):
+		self.tvshows = load_tvshows('shows.txt')
+	
+	def delete_shows(self):
+		self.tvshows = {}
+	
+	def delete_show(self, sid):
+		if sid in self.tvshows:
+			self.tvshows.pop(sid)
