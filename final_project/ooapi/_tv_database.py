b9a49c6fe16067e6272f0d9f68912567a2bb773a
Rayyan Karim <rkarim0598@gmail.com>
2018-11-05 18:06:51 -0500

    Add database methods for shows

--- final_project/ooapi/_tv_database.py
+++ final_project/ooapi/_tv_database.py
@@ -20,9 +20,42 @@ class _tv_database:
 		self.tvshows = {}
 		for line in f:
 			line = line.rstrip()
-			components = line.split(":")
-			'''
-			etc
-			'''
-			self.tvshows[tvid] = ''' SOMETHING '''
+			components = line.split("::")
+
+                        self.tvshows[components[0]] = {}
+			self.tvshows[components[0]]['name'] = components[1]
+                        self.tvshows[components[0]]['genres'] = components[2]
+                        self.tvshows[components[0]]['site'] = components[3]
+                        self.tvshows[components[0]]['rating'] = components[4]
+                        self.tvshows[components[0]]['image'] = components[5]
+                        self.tvshows[components[0]]['summary'] = components[6]
+                        
 		return self.tvshows
+        
+        def get_show(self, sid):
+                sid = str(sid)
+                
+                if sid in self.tvshows:
+                        output = self.tvshows[sid]
+                        output['result'] = 'success'
+                else:
+                        output = { 'result': 'failure' }
+
+                return output
+
+        def set_show(self, sid, d = {}):
+                if len(d) == 6:
+                        self.tvshows[sid] = d
+
+        def get_shows(self):
+                return [i for i in self.tvshows.keys()]
+
+        def reset_shows(self):
+                self.tvshows = load_tvshows('shows.txt')
+
+        def delete_shows(self):
+                self.tvshows = {}
+
+        def delete_show(self, sid):
+                if sid in self.tvshows:
+                        self.tvshows.pop(sid)
