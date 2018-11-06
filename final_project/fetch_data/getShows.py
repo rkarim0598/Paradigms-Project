05c227fb0843ce9921d5a2347e1e1ab400918824
Rayyan Karim <rkarim0598@gmail.com>
2018-11-05 20:25:40 -0500

    Updated shows.txt

--- final_project/fetch_data/getShows.py
+++ final_project/fetch_data/getShows.py
@@ -8,7 +8,30 @@ content = json.loads(r.content.decode())
 f = open('shows.txt', 'w')
 
 for show in content:
-    sid = show['id']
-    sname = show['name']
+    slist = []
+    slist.append(str(show['id']))
+    slist.append(show['name'])
 
-    f.write(str(sid) + ',' + sname + '\n')
+    strGenre = ''
+
+    if not show['genres']:
+        strGenre = "None"
+    else:
+        for genre in show['genres']:
+            if genre != show['genres'][0]:
+                strGenre += '|' + genre
+            else:
+                strGenre += genre
+    
+    slist.append(strGenre)
+
+    if not show['officialSite']:
+        slist.append(show['url'])
+    else:
+        slist.append(show['officialSite'])
+    slist.append(str(show['rating']['average']))
+    slist.append(show['image']['medium'])
+    slist.append(show['summary'])
+
+    result = '::'.join(slist) + '\n'
+    f.write(result)
