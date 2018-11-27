97b948e50018470c0d3e04c36f51811e3a81bbe2
Ryker Campbell McIntyre <rmcinty3@student04.cse.nd.edu>
2018-11-27 16:27:24 -0500

    added test all script

---
+++ final_project/webserver/tests/test_all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+printf "testing /shows/ and /shows/:show_id\n"
+python test_shows.py
+
+printf "\ntesting /ratings/:show_id\n"
+python test_ratings.py
+
+printf "\ntesting /recommendations/:user_id\n"
+python test_rec.py
+
+printf "\ntesting /users/:user_id\n"
+python test_users.py
+
+printf "\ntesting /reset/ and /reset/:show_id\n"
+python test_reset.py
