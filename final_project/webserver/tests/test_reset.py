5389323d57e3f514b3639ecc5edf5c115664b406
Ryker Campbell McIntyre <rmcinty3@student04.cse.nd.edu>
2018-11-27 15:44:05 -0500

    wrote test for reset, it works

---
+++ final_project/webserver/tests/test_reset.py
@@ -0,0 +1,38 @@
+import unittest
+import requests
+import json
+
+class TestReset(unittest.TestCase):
+
+	SITE_URL = 'http://student04.cse.nd.edu:52094'
+	RESET_URL = SITE_URL + '/reset/'
+	SHOWS_URL = SITE_URL + '/shows/'
+
+	def test_reset_data(self):
+		m = {}
+		r = requests.put(self.RESET_URL, data = json.dumps(m))
+		r = json.loads(r.text)
+		self.assertEqual(r['result'], 'success')
+	
+	def test_reset_show(self):
+		m = {}
+		r = requests.get(self.SHOWS_URL + str(1))
+		r = json.loads(r.text)['output']
+		self.assertEqual(r['name'], 'Under the Dome')
+		r['name'] = 'Something Else'
+		r = requests.put(self.SHOWS_URL + str(1), data = json.dumps(r))
+		r = json.loads(r.text)
+		self.assertEqual(r['result'], 'success')
+		r = requests.get(self.SHOWS_URL + str(1))
+		r = json.loads(r.text)['output']
+		self.assertEqual(r['name'], 'Something Else')
+		r = requests.put(self.RESET_URL + str(1), data = json.dumps(m))
+		r = json.loads(r.text)
+		self.assertEqual(r['result'], 'success')
+		r = requests.get(self.SHOWS_URL + str(1))
+		r = json.loads(r.text)['output']
+		self.assertEqual(r['name'], 'Under the Dome')
+		
+
+if __name__ == "__main__":
+	unittest.main()
