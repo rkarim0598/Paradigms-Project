64627b2ce9294d9ea3b053cc447ac262dc4f0664
Rayyan Karim <rkarim0598@gmail.com>
2018-11-29 00:48:43 -0500

    Generate header dynamically

--- final_project/website/scripts/header.js
+++ final_project/website/scripts/header.js
@@ -1,3 +1,40 @@
+initHeader = () => {
+    var header = document.createElement('header')
+    header.setAttribute('class', 'mdl-layout__header')
+
+    var div = document.createElement('div')
+    div.setAttribute('class', 'mdl-layout__header-row')
+
+    var h1 = document.createElement('h1')
+    h1.setAttribute('class', 'mdl-layout-title')
+    h1.innerHTML = 'Show Wiz'
+
+    var spacer = document.createElement('div')
+    spacer.setAttribute('class', 'mdl-layout-spacer')
+
+    var nav = document.createElement('nav')
+    nav.setAttribute('class', 'mdl-navigation')
+
+    var signIn = document.createElement('a')
+    signIn.setAttribute('id', 'sign')
+    signIn.href = 'sign-or-create.html'
+    signIn.setAttribute('class', 'mdl-navigation__link mdl-layout-spacer')
+    signIn.innerHTML = 'Sign In/Create Account'
+
+    var browse = document.createElement('a')
+    browse.href = 'browse.html'
+    browse.setAttribute('class', 'mdl-navigation__link mdl-layout-spacer')
+    browse.innerHTML = 'Browse'
+
+    nav.appendChild(signIn)
+    nav.append(browse)
+
+    div.appendChild(h1)
+    div.appendChild(spacer)
+    div.appendChild(nav)
+    header.appendChild(div)
+    document.getElementById('container').prepend(header)
+}
 // call this function after user signs in/clicks sign out
 updateUser = () => {
     var anchor = document.getElementById('sign');
