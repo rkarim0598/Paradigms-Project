c224b740f962d022a4cf95d0a7454adc0df0d05b
Rayyan Karim <rkarim0598@gmail.com>
2018-11-30 11:10:20 -0500

    Add sign in capability

--- final_project/website/scripts/sign-or-create.js
+++ final_project/website/scripts/sign-or-create.js
@@ -1,2 +1,33 @@
 initHeader()
 updateUser()
+
+verify = (uid, password) => {
+    var xhr = new XMLHttpRequest()
+    xhr.open('GET', 'http://student04.cse.nd.edu:52048/users/' + uid, true)
+    xhr.onload = () => {
+        var response = JSON.parse(xhr.responseText)
+        console.log(response)
+        if (response['result'] == 'success') {
+            localStorage.setItem('uid', uid)
+            window.location.href = '../website/browse-alt.html'
+        }
+    }
+
+    xhr.send()
+}
+
+sign = () => {
+    var uid = document.getElementById('sign-uid').value
+    var password = document.getElementById('sign-password').value
+
+    // don't execute if no uid or password entered
+    if (uid == "" || password == "") {
+        return;
+    }
+
+    verify(uid, password)
+
+}
+
+var signIn = document.getElementById('sign-button')
+signIn.addEventListener("click", sign)
